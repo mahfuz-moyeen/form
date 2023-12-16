20cdcbeb0b7629ee11a2711edb67015152dc7ccf
Mahfuz Moyeen <96814940+mahfuz-moyeen@users.noreply.github.com>
2023-12-17 03:06:53 +0600

    deploy vercel

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# React + Vite
+# Form use React.js
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+install file : `npm i` or `yarn`
+run file on localhost : `npm run dev` or `yarn dev`
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## use item:
+- React.js
+- React toastify
+- Tailwind css
+- use API from backend database
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,78 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+
 
 const Home = () => {
     const [checked, setChecked] = useState(false);
+    const [sectorsData, setSectorsData] = useState([])
+    const [formData, setFormData] = useState({})
+    const [refetch, setRefetch] = useState(null)
+    const [isUpdate, setIsUpdate] = useState(false)
+
+
+    useEffect(() => {
+        fetch('https://form-server-six.vercel.app/sectors')
+            .then(res => res.json())
+            .then(data => {
+                setSectorsData(data)
+            })
+    }, [])
+
+    useEffect(() => {
+        fetch('https://form-server-six.vercel.app/form')
+            .then(res => res.json())
+            .then(data => {
+                setFormData(data)
+                setIsUpdate(data?.name)
+            })
+    }, [refetch])
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
 
     const onSubmit = (e) => {
         e.preventDefault();
         const name = e.target.name.value;
         const sector = e.target.sector.value;
-        const formData = { name, sector, checked }
-
-        toast.success(`Form updated successfully`)
+        const updateData = { ...formData, name, sector, terms: checked }
+        fetch('https://form-server-six.vercel.app/form-update',
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: 'PATCH',
+                body: JSON.stringify(updateData)
+            }
+        )
+            .then(res => res.json())
+            .then(data => {
+                toast.success(data.message);
+                setRefetch(data)
+            })
     }
 
     const handleClear = () => {
         toast.error(`Form clear successfully`)
+        fetch('https://form-server-six.vercel.app/form-clear',
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: 'DELETE',
+            }
+        )
+            .then(res => res.json)
+            .then(data => {
+                toast.error(data.message);
+                setRefetch(data)
+
+            })
     }
 
     return (
@@ -25,14 +83,17 @@ const Home = () => {
                     Please enter your name and pick the Sectors you are currently involved in.
                 </h3>
                 <form onSubmit={onSubmit}>
+
                     {/* name   */}
                     <div>
                         <label className='text-[16px] text-black font-semibold'>Name:</label>
                         <input
                             type="text"
                             name='name'
+                            value={formData.name}
                             className='rounded-[10px] bg-[#EEE] p-2 text-black w-full mt-2'
                             placeholder='Enter your name'
+                            onChange={handleChange}
                             required
                         />
                     </div>
@@ -43,12 +104,42 @@ const Home = () => {
                         <select
                             name="sector"
                             className='rounded-[10px] bg-[#EEE] p-2 text-black w-full mt-2'
+                            value={formData.sector}
+                            onChange={handleChange}
                             required
                         >
                             <option selected disabled value=''>Select your sectors</option>
-                            <option value='1'>1</option>
-                            <option value='2'>2</option>
-                            <option value='3'>3</option>
+                            {
+                                sectorsData.map(item => <React.Fragment
+                                    key={item?.name}
+                                >
+                                    <option disabled className='font-bold'>{item?.name}</option>
+                                    {
+                                        item?.values?.length > 0 && item?.values?.map((x, i) => <React.Fragment
+                                            key={x + i}
+                                        >
+                                            {
+                                                x?.subName ? <React.Fragment>
+                                                    <option
+                                                        value={x?.subName}
+                                                        style={{ textIndent: '100px' }}
+                                                    >&nbsp;&nbsp;&nbsp;&nbsp;{x?.subName}</option>
+                                                    {
+                                                        x?.subValues?.length > 0 && x.subValues.map((y, index) =>
+                                                            <option
+                                                                key={y + index}
+                                                                value={y}
+                                                            >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{y} </option>
+                                                        )}
+                                                </React.Fragment>
+                                                    : <option value={x}>&nbsp;&nbsp;&nbsp;&nbsp;{x} </option>
+                                            }
+                                        </React.Fragment>)
+                                    }
+                                </React.Fragment>
+
+                                )
+                            }
                         </select>
                     </div>
 
@@ -61,30 +152,45 @@ const Home = () => {
                             type="checkbox"
                             name='terms'
                             className='rounded scale-110'
-                            checked={checked}
+                            // checked={formData.checked || checked}
+                            defaultChecked={checked || formData.terms}
                             required
                         />
                         <label className='text-[16px] text-black font-semibold cursor-pointer'>Agree to terms</label>
                     </div>
 
+                    {/* button  */}
                     <div className='flex mt-6 items-center gap-4'>
-                        <button
-                            type='submit'
-                            className='bg-violet-800 py-2 px-5 rounded-[10px] text-white font-bold text-[18px]'
-                        >
-                            Save
-                        </button>
-                        <button
-                            type='button'
-                            className='bg-red-500 py-2 px-5 rounded-[10px] text-white font-bold text-[18px]'
-                            onClick={handleClear}
-                        >
-                            Clear
-                        </button>
+                        {
+                            isUpdate ?
+                                <>
+                                    <button
+                                        type='submit'
+                                        className='bg-violet-800 py-2 px-5 rounded-[10px] text-white font-bold text-[18px]'
+                                    >
+                                        Update
+                                    </button>
+                                    <button
+                                        type='button'
+                                        className='bg-red-500 py-2 px-5 rounded-[10px] text-white font-bold text-[18px]'
+                                        onClick={handleClear}
+                                    >
+                                        Clear
+                                    </button>
+                                </>
+                                :
+                                <button
+                                    type='submit'
+                                    className='bg-violet-800 py-2 px-5 rounded-[10px] text-white font-bold text-[18px]'
+                                >
+                                    Save
+                                </button>
+
+                        }
                     </div>
                 </form>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
